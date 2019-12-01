36e698fa8c75bee289e319957ce7191323d10128
Deepank Girish <50998449+gdeepank@users.noreply.github.com>
2019-12-01 11:18:38 +0530

    Add files via upload
    Project developed by Deepank, Siddharth and Pruthvi

---
+++ functions.php
@@ -0,0 +1,43 @@
+<?php // Example 26-1: functions.php
+  $dbhost  = 'localhost';   
+  $dbname  = 'records';   
+  $dbuser  = 'deepank';   
+  $dbpass  = 'deepank';
+  $appname = 'Project Heka';   
+
+  $connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+  if ($connection->connect_error) die($connection->connect_error);
+
+  function createTable($name, $query)
+  {
+    queryMysql("CREATE TABLE IF NOT EXISTS $name($query)");
+    echo "Table '$name' created or already exists.<br>";
+  }
+
+  function queryMysql($query)
+  {
+    global $connection;
+    $result = $connection->query($query);
+    if (!$result) die($connection->error);
+    return $result;
+  }
+
+  function destroySession()
+  {
+    $_SESSION=array();
+
+    if (session_id() != "" || isset($_COOKIE[session_name()]))
+      setcookie(session_name(), '', time()-2592000, '/');
+
+    session_destroy();
+  }
+
+  function sanitizeString($var)
+  {
+    global $connection;
+    $var = strip_tags($var);
+    $var = htmlentities($var);
+    $var = stripslashes($var);
+    return $connection->real_escape_string($var);
+  }
+?>
